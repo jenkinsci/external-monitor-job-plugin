14758d66c9013bb93b8ebe6624f76bacada3442a
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-05-29 17:35:21 -0700

    Moving another test code
    from test/src/test/java/hudson/model/GetEnvironmentOutsideBuildTest.java

--- src/test/java/hudson/model/ExternalRunTest.java
+++ src/test/java/hudson/model/ExternalRunTest.java
@@ -1,7 +1,13 @@
 package hudson.model;
 
+import hudson.EnvVars;
+import hudson.util.StreamTaskListener;
+import jenkins.model.Jenkins;
+import org.junit.Assert;
+import org.jvnet.hudson.test.Bug;
 import org.jvnet.hudson.test.HudsonTestCase;
 
+import java.io.IOException;
 import java.io.StringReader;
 
 /**
@@ -22,5 +28,32 @@ public class ExternalRunTest extends HudsonTestCase {
             "<run><log content-encoding='UTF-8'>AAAAAAAA</log><result>1</result>"
         ));
         assertEquals(b.getResult(),Result.FAILURE);
+    }
+
+    @Bug(11592)
+    public void testExternalJob() throws Exception {
+        ExternalJob p = jenkins.createProject(ExternalJob.class, createUniqueProjectName());
+        ExternalRun b = p.newBuild();
+        b.acceptRemoteSubmission(new StringReader(
+            "<run><log content-encoding='UTF-8'></log><result>0</result><duration>1</duration></run>"
+        ));
+
+        assertGetEnvironmentWorks(b);
+    }
+
+    @SuppressWarnings("rawtypes")
+    private void assertGetEnvironmentWorks(Run build) throws IOException, InterruptedException {
+        whenJenkinsMasterHasNoExecutors();
+        // and getEnvironment is called outside of build
+        EnvVars envVars =  build.getEnvironment(StreamTaskListener.fromStdout());
+        // then it should still succeed - i.e. no NPE o.s.l.t.
+        assertNotNull(envVars);
+    }
+
+    private void whenJenkinsMasterHasNoExecutors() throws IOException {
+        Jenkins.getInstance().setNumExecutors(0);
+        // force update of nodes:
+        Jenkins.getInstance().setNodes(Jenkins.getInstance().getNodes());
+        Assert.assertNull(Jenkins.getInstance().toComputer());
     }
 }
